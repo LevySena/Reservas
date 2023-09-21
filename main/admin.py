85baaad3bbd2874d0edf53a9402826cfb6f58b29
LevySena <103257708+LevySena@users.noreply.github.com>
2023-09-21 16:04:42 -0300

    Criação do código pelo django junto com models

---
+++ main/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from main.models import Quartos,Cliente,Ocupacao,Quarto_Cat,Ocupacao_tipo
+# Register your models here.
+admin.site.register(Quartos)
+admin.site.register(Cliente)
+admin.site.register(Ocupacao)
+admin.site.register(Quarto_Cat)
+admin.site.register(Ocupacao_tipo)
